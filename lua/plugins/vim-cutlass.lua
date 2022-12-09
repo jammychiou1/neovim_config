8786ccd7848e87daf4c4f61b40c66c616e2c507c
jammychiou1 <jammy.chiou1@gmail.com>
2022-12-09 17:31:56 +0800

    fixed p in visual mode to work at end of line

--- lua/plugins/vim-cutlass.lua
+++ lua/plugins/vim-cutlass.lua
@@ -6,16 +6,16 @@ end
 
 -- normal mode
 noremap('n', {
-    ['x'] = 'd', -- x{motion}    => cut text that {motion} moves over
-    ['xx'] = 'dd', -- xx         => cut this line
-    ['X'] = 'D', -- X            => cut to end of line
+    ['x'] = 'd', -- x{motion}           => cut text highlighted {motion} highlighted over
+    ['xx'] = 'dd', -- xx                => cut this line
+    ['X'] = 'D', -- X                   => cut to end of line
 })
 
 -- visual mode
 noremap('x', {
-    ['x'] = 'd', -- {Visual}x    => cut highlighted text
+    ['x'] = 'd', -- {Visual}x           => cut highlighted text
 })
 
 noremap('x', {
-    ['p'] = '"_P', -- {Visual}p  =>  replace highlighted text
+    ['p'] = '"_c<C-r>"', -- {Visual}p   => replace highlighted text
 })
