80664095c97404f68307123bb1a5636d69ca88f8
jammychiou1 <jammy.chiou1@gmail.com>
2022-12-13 10:54:30 +0800

    added neoscroll

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -268,4 +268,10 @@ require('packer').startup(function(use)
             require('which-key').setup()
         end
     }
+    use {
+        'karb94/neoscroll.nvim',
+        config = function()
+            require('neoscroll').setup()
+        end
+    }
 end)
