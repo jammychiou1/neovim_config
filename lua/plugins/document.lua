f648f5fd523e1273cd5cdc7b13300c73bdf960d0
jammychiou1 <jammy.chiou1@gmail.com>
2025-03-28 17:28:02 +0800

    re-enable vimtex match paren

--- lua/plugins/document.lua
+++ lua/plugins/document.lua
@@ -5,7 +5,6 @@ return {
         ft = 'tex',
         init = function()
             vim.g.vimtex_mappings_enabled = false
-            vim.g.vimtex_matchparen_enabled = false
             vim.g.vimtex_imaps_enabled = false
             vim.g.vimtex_compiler_enabled = false
         end,
