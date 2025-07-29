d39a2a0b3cd88f20c0c941bc7dbbc0f13c8c3111
jammychiou1 <jammy.chiou1@gmail.com>
2025-07-29 21:45:16 +0800

    Remove outdated setup dict for better_escape

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -45,11 +45,7 @@ return {
         'max397574/better-escape.nvim',
         event = "InsertEnter",
         config = function()
-            require('better_escape').setup({
-                keys = function()
-                    return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
-                end,
-            })
+            require('better_escape').setup()
         end,
     },
     {
